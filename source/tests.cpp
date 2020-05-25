653950f92d29a5766dce4244548fb0cfd4dca0f7
McKraska <McKraska@users.noreply.github.com>
2020-05-25 15:24:28 +0200

    vec.cpp und vec.hpp erstellt
    vec.cpp und vec.hpp erstellt: Dazu erste versuche mit Catch funktion die Custom Vec operatoren ein zu setzen

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,26 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "vec2.hpp"
+
+
+Vec2 a;
+Vec2 b{ 5.1f, -9.3f };
+Vec2 c{};
+
+Vec2 checkvec(Vec2 c) {
+
+	Vec2 t1{ 1.0f, -1.0 };
+	Vec2 t2{ t1 + c };
+	
+	return t2;
+
+}
+
+TEST_CASE("describe_checkvec", "[checksum]") {
+
+	REQUIRE(checkvec(5.0f, -10.0f) == 6.0f, -11.0f);
+
+}
 
 int main(int argc, char *argv[])
 {
