18016f3ef671b9b432037034051fdd8946e6bfcf
McKraska <McKraska@users.noreply.github.com>
2020-05-26 21:20:37 +0200

    Gemerkt das alles Falsch war
    Gemerkt das alles Falsch war und mit Cpp implementation neu angefangen

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -1,1 +1,37 @@
 #include "vec2.hpp"
+
+Vec2& Vec2::operator +=(Vec2 const& v){
+
+}
+
+Vec2& Vec2::operator -=(Vec2 const& v) {
+
+}
+
+Vec2& Vec2::operator *=(float s) {
+
+}
+
+Vec2& Vec2::operator /=(float s) {
+
+}
+
+Vec2 operator +(Vec2 const& u, Vec2 const& v) {
+
+}
+
+Vec2 operator -(Vec2 const& u, Vec2 const& v) {
+
+}
+
+Vec2 operator *(Vec2 const& v, float s) {
+
+}
+
+Vec2 operator /(Vec2 const& v, float s) {
+
+}
+
+Vec2 operator *(float s, Vec2 const& v) {
+
+}
